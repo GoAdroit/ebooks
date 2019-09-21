ef254a3f198f720e4e0dca22897ca5682ce70b91
Prasanna <kaprasannagt@gmail.com>
2019-09-21 21:01:44 +0530

    Added class PDFAPi to achieve creation of sample PDF is complete Added itext dependency

---
+++ ebook-service-war/src/main/java/com/adroit/ebooks/pdf/PDFAPI.java
@@ -0,0 +1,52 @@
+package com.adroit.ebooks.pdf;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfSmartCopy;
+
+/**
+ * All PDF related APIs 
+ * @author praku
+ *
+ */
+public class PDFAPI {
+	private static final String SAMPLEFILENAME = "-sample";
+
+	public static File createSample(File originalPDF, String fileName, String dir) {
+		File sampleFile = new File(dir.concat(File.separator).concat(fileName).concat(SAMPLEFILENAME).concat(".pdf"));
+
+		try {
+			Document document;
+			PdfReader reader = new PdfReader(Files.readAllBytes(originalPDF.toPath()));
+			int numOfPagesInOriginalPDF = reader.getNumberOfPages();
+			if(numOfPagesInOriginalPDF > 100) {
+
+				document = new Document();
+				PdfSmartCopy pdfCopy = new PdfSmartCopy(document, new BufferedOutputStream(new FileOutputStream(sampleFile)));
+				document.open();
+
+				// add first 10 pages for sure
+				for (int p = 1; p < 12; p++) {
+					PdfImportedPage page = pdfCopy.getImportedPage(reader, p);
+					pdfCopy.addPage(page);
+				}
+				
+				document.close();
+				
+				return sampleFile;
+			}
+		} catch (IOException | DocumentException e) {
+			e.printStackTrace();
+			// TODO LOG
+		}
+		return sampleFile;
+	}
+}
--- ebook-service-war/src/main/java/com/adroit/ebooks/service/UploadServlet.java
+++ ebook-service-war/src/main/java/com/adroit/ebooks/service/UploadServlet.java
@@ -1,5 +1,6 @@
 package com.adroit.ebooks.service;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import com.adroit.ebooks.pdf.PDFAPI;
+
 /**
  * Servlet implementation class UploadServlet
  */
@@ -25,11 +28,11 @@ public class UploadServlet extends HttpServlet {
 	private static final String PART_NAME = "kpUpload";
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public UploadServlet() {
-    }
+	/**
+	 * Default constructor. 
+	 */
+	public UploadServlet() {
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -42,32 +45,44 @@ public class UploadServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("============Receiving file");
-		
+		System.out.println("ebook100 ============Receiving file");
+
 		// Retrieves <input type="file" name="file" multiple="true">
-        System.out.println("============Request parts : " + request.getParts().size());
+		System.out.println("ebook100 ============Request parts : " + request.getParts().size());
 		List<Part> fileParts = request.getParts().stream().filter(new Predicate<Part>() {
 			@Override
 			public boolean test(Part part) {
-				System.out.println("============part.getName()" + part.getName());
+				System.out.println("ebook100 ============part.getName()" + part.getName());
 				return PART_NAME.equals(part.getName());
 			}
 		}).collect(Collectors.toList());
-		System.out.println("============fileParts : " + fileParts.size());
-		
+		System.out.println("ebook100 ============fileParts : " + fileParts.size());
+
 		if(!(fileParts.size() >= 1)) {
 			response.getWriter().append("File upload failed");
 		}
 
-	    for (Part filePart : fileParts) {
-	        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-	        System.out.println("============FileName : " + fileName);
-	        response.getWriter().append("File uploaded successfully : " + fileName + "\n");
-	        InputStream fileIs = filePart.getInputStream();
-	        FileOutputStream fos = new FileOutputStream(fileName);
-	        fileIs.transferTo(fos);
-	        // TODO Log creation of file with details
-	    }
+		for (Part filePart : fileParts) {
+			String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+			String tempDir = String.valueOf(request.getServletContext().getAttribute("javax.servlet.context.tempdir"));
+			System.out.println("ebook100 ============FileName of uploaded file : " + fileName);
+			File uploadedFile = new File(tempDir.concat(File.separator).concat(fileName));
+
+			InputStream fileIs = filePart.getInputStream();
+			FileOutputStream fos = new FileOutputStream(uploadedFile);
+			fileIs.transferTo(fos);
+			System.out.println("ebook100 Uploaded file path : " + uploadedFile.getAbsolutePath());
+
+			// TODO Use EHCache
+
+			// TODO Create a shortened version of the PDF
+			File shortenedPDF = PDFAPI.createSample(uploadedFile, fileName, tempDir);
+			System.out.println("ebook100 shortenedPDF file path : " + shortenedPDF.getAbsolutePath());
+
+			// TODO Save both files to DB
+
+			response.getWriter().append("File uploaded successfully : " + fileName + "\n");
+		}
 	}
 
 }
